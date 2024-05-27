e982a9e896386df1925ae36fbd824fe0f06c982f
JackBlaaack <eugene.lapytko@gmail.com>
2024-05-27 10:54:58 +0300

    Updated JS_2

--- JS_2/task3.js
+++ JS_2/task3.js
@@ -1,22 +1,26 @@
 // Преобразовать Task 2* таким образом, чтобы age принимался через prompt в привязанной вёрстке, а результат выводился в alert
-const age = +prompt("Enter your age, please")
+const age = +prompt("Enter your age, please");
 const age_2 = 18;
 const age_3 = 60;
 switch (true) {
-    case age < age_2:
-      alert(
-        `You don’t have access cause your age is ${age}. It’s less then ${age_2}`
-      );
-      break;
-  
-    case age >= age_2 && age < age_3:
-      alert("Welcome!");
-      break;
-  
-    case age > age_3:
-      alert("Keep calm and look Culture channel");
-      break;
-  
-    default:
-      alert("Technical work");
-  }
+  case 0:
+  case -1:
+  case "my age":
+    console.log("Invalid age");
+  case age < age_2:
+    alert(
+      `You don’t have access cause your age is ${age}. It’s less then ${age_2}`
+    );
+    break;
+
+  case age >= age_2 && age < age_3:
+    alert("Welcome!");
+    break;
+
+  case age > age_3:
+    alert("Keep calm and look Culture channel");
+    break;
+
+  default:
+    alert("Technical work");
+}
